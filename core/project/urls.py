05121d1ba5679a3fab8e6f3bd19d0e567fc93b6c
Marcos Santos <marcosdiego.ns01@gmail.com>
2023-07-17 12:27:00 -0500

    feat: register routes from statement view set

--- core/project/urls.py
+++ core/project/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from core.apps.statement.views.statement_view_set import StatementViewSet
+
+router = routers.DefaultRouter()
+router.register(r'statement', StatementViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
