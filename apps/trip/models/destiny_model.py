336139ca944920f322b353740930adc14f9e9adc
Marcos Santos <marcosdiego.ns01@gmail.com>
2023-07-24 09:34:35 -0500

    refactor: change photo field to accept blank value

--- apps/trip/models/destiny_model.py
+++ apps/trip/models/destiny_model.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Destiny(models.Model):
-    photo = models.ImageField(upload_to='destiny_photos')
+    photo = models.ImageField(upload_to='destiny_photos', blank=True)
     name = models.CharField(max_length=150, blank=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
