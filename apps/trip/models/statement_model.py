75a3852b7fe4f0b99e1e6aeb55e51d781e587e13
Marcos Santos <marcosdiego.ns01@gmail.com>
2023-07-24 10:16:19 -0500

    refactor: change statement photo field to accept blank value

--- apps/trip/models/statement_model.py
+++ apps/trip/models/statement_model.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Statement(models.Model):
-    photo = models.ImageField(upload_to='statement_photos')
+    photo = models.ImageField(upload_to='statement_photos', blank=True)
     text = models.TextField()
     person = models.CharField(max_length=100, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
